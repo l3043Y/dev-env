01cb2c91e2f8583e638ecd6bcdbd921f55b8fcd2
l3043y <boreyc2013@gmail.com>
2022-06-15 10:15:35 +0700

    changes

---
+++ arc/ind.ps1
@@ -0,0 +1,24 @@
+choco install telegram -y
+choco install brave -y
+
+choco install vscode -y
+choco install nvm -y
+choco install notepadplusplus.install -y
+choco install docker-desktop -y
+choco install openssh -y
+choco install git.install -y
+choco install anaconda3
+
+choco install powertoys
+choco install devtoys
+choco install vnc-viewer -y
+choco install openvpn -y
+choco install anydesk -y
+choco install 7zip
+
+choco install vlc -y
+choco install spotify -y
+choco install chocolateygui -y
+
+choco install pdfxchangeeditor -y
+choco install googlechrome -y
